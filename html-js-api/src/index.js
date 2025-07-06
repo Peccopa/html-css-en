c82403f3044c953c8fe156fe8f010c5f3d6b0c69
Andrey Zharkikh <orbital.logistics@yandex.ru>
2025-07-06 10:08:32 +0300

    feat: add html-css-en-06-table

---
+++ html-js-api/src/index.js
@@ -0,0 +1,44 @@
+import '../assets/css/style.css';
+
+const app = document.getElementById('app');
+app.innerHTML = `
+  <h1>JavaScript HTML5 APIs</h1>
+  <div class="uploader">
+    <div class="dragme" draggable="true"></div>
+    <div class="dropzone">🎯 Drag here!</div>
+  </div>
+
+  <style>
+  .uploader {
+    box-sizing: border-box;
+    max-width: 90%;
+    border-radius: 10px;
+    border-bottom: 3px solid #d2d5da;
+    margin: 25px auto;
+    padding: 25px;
+    background: #fff;
+  }
+  .dropzone {
+    border-radius: 5px;
+    margin-top: 25px;
+    padding: 25px;
+    border: 2px dashed #d2d5da;
+    background: #f1f2f5;
+  }
+  .dragme {
+    background: #ce1f99;
+    border-radius: 5px;
+    width: 50px;
+    height: 50px;
+  }
+  </style>
+`;
+
+// const div = document.createElement('div');
+// console.dir('draggable' in div);
+
+const init = () => {};
+
+if ('draggable' in document.createElement('div')) {
+  init();
+}
